91a84fbf584f0fe86ad44656cbbe2cf93ac2f725
ManobhavSachan <manobhavsachan@gmail.com>
2023-11-10 13:39:05 +0530

    Sending Image using API

--- App/App.js
+++ App/App.js
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, Image, Button, StyleSheet, TextInput } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import axios from 'axios';
-
 const App = () => {
   const [image, setImage] = useState(null);
   const [uploadImage, setUploadImage] = useState(null); // [image, setImage
   const [annotatedImage, setAnnotatedImage] = useState(null);
   const [upscaling, setUpScaling] = useState('1');
   const [threshold, setThreshold] = useState('0.6');
-
+  
   const selectImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -17,42 +16,60 @@ const App = () => {
       aspect: [4, 3],
       quality: 1,
     });
-    // console.log(result.assets[0].uri);
+    console.log(result.assets[0].uri);
     // setImage(result);
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
       
-     
+      convertToBlob(result.assets[0]);
+
       // const blob = await result.assets[0].uri.blob();
       setUploadImage(result);
       setAnnotatedImage(null);
     }
     // console.log(image);
   };
-
+  const convertToBlob = (image) => {
+    fetch(image.uri)
+      .then(response => response.blob())
+      .then(blob => {
+        // 'blob' contains the image in Blob format
+        // Use 'blob' for further processing (e.g., sending via Axios or other uses)
+        setUploadImage(blob);
+      })
+      .catch(error => {
+        console.error('Error converting image to blob:', error);
+      });
+  };
   const processImage = async () => {
     if (image) {
       let formData = new FormData();
       const match = new RegExp(/image\/(\w+)\b/).exec(image);
+      // console.log('Match === ', match[1]);
       // formData.append('image', {
       //   uri: image,
-      //   type: `image/${match[1]}`,
+      //   type: match[0],
       //   name: `image.${match[1]}`,
       // });
+      console.log(uploadImage);
       formData.append('image', uploadImage);
       formData.append('upscaling', upscaling);
       formData.append('threshold', threshold);
-      console.log(formData);
+      console.log(formData.image);
       try {
-        const response = await axios.post('http://48ac-35-202-194-47.ngrok-free.app/annotate_image', formData, {
+        const response = await axios.post('http://d314-35-202-194-47.ngrok-free.app/annotate_image', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
-        });
-        console.log("response = ", response);
-
-        setAnnotatedImage(response.data);
+        }, { responseType: 'blob' });
+        console.log("response = ", response.data);
+        // const blob = new Blob([response.data], { type: 'image/png' });
+        // console.log(blob);
+        const uri = URL.createObjectURL(response.data);
+        console.log(uri);
+        setAnnotatedImage( uri );
+        // setAnnotatedImage({ uri: `data:image/png;base64,${response.data.image}` });
       } catch (error) {
         console.error('Error processing image:', error);
       }
@@ -78,7 +95,7 @@ const App = () => {
         value={threshold}
       />
       {image && <Image source={{ uri: image }} style={styles.image} />}
-      {annotatedImage && <Image source={{ uri: `data:image/png;base64,${annotatedImage}` }} style={styles.image} />}
+      {annotatedImage && <Image source={{ uri: annotatedImage }} style={styles.image} />}
     </View>
   );
 };
