e9c7318983b85be014fdbb14e31e28e3de0a5103
ManobhavSachan <manobhavsachan@gmail.com>
2023-11-12 15:48:31 +0530

    Fixed Displaying Received Image Error

--- App/App.js
+++ App/App.js
@@ -13,7 +13,7 @@ const App = () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      // aspect: [4, 3],
       quality: 1,
     });
     console.log(result.assets[0].uri);
@@ -56,25 +56,68 @@ const App = () => {
       formData.append('image', uploadImage);
       formData.append('upscaling', upscaling);
       formData.append('threshold', threshold);
+      // console.log('working');
       console.log(formData.image);
+      // console.log('working');
+      
+      
+      // fetchData();
+
+
       try {
-        const response = await axios.post('http://d314-35-202-194-47.ngrok-free.app/annotate_image', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        }, { responseType: 'blob' });
-        console.log("response = ", response.data);
+
+          const response = await axios.post('http://127.0.0.1:5000/annotate_image', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+            // responseType: 'arraybuffer', // Ensure correct response type for binary data
+          });
+          // const base64String = Buffer.from(response.data, 'binary').toString('base64');
+    // console.log(base64String);
+        // console.log("response = ", response);
+        // const data = await response.json();
+          console.log(JSON.stringify(response.data));
         // const blob = new Blob([response.data], { type: 'image/png' });
         // console.log(blob);
-        const uri = URL.createObjectURL(response.data);
-        console.log(uri);
-        setAnnotatedImage( uri );
+        // const uri = URL.createObjectURL(response.data);
+        // const base64 = `data:image/png;base64,${arrayBufferToBase64(response.data)}`;       
+        // // console.log(uri);
+        // console.log(base64);
+        // setAnnotatedImage( {uri : base64} );
+        console.log({uri: annotatedImage});
+        setAnnotatedImage(`data:image/jpeg;base64,${response.data.image}`);
         // setAnnotatedImage({ uri: `data:image/png;base64,${response.data.image}` });
       } catch (error) {
-        console.error('Error processing image:', error);
+        console.error('Error processing image:', JSON.stringify(error));
       }
     }
     else console.log('No image selected');
+  };
+
+  const arrayBufferToBase64 = (buffer) => {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    console.log(bytes);
+    for (let i = 0; i < bytes.byteLength; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    console.log((binary));
+    return btoa(binary);
+  };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://any-anime.p.rapidapi.com/v1/anime/gif/1', {
+        headers: {
+          'X-RapidAPI-Key': '05685095e9mshb620c5b10f74fb4p173370jsnbcd9556cf0be',
+          'X-RapidAPI-Host': 'any-anime.p.rapidapi.com'
+        }
+      });
+      console.log(response.data);
+      setAnnotatedImage({ uri: response.data.images[0] });
+      console.log(annotatedImage);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -95,7 +138,7 @@ const App = () => {
         value={threshold}
       />
       {image && <Image source={{ uri: image }} style={styles.image} />}
-      {annotatedImage && <Image source={{ uri: annotatedImage }} style={styles.image} />}
+      {annotatedImage && <Image source={{uri : annotatedImage}} style={styles.image} />}
     </View>
   );
 };
